f94a7d58713db3f37a9d8783322947c51577edde
fantesia <fantesia@163.com>
2017-06-19 18:04:06 +0800

    Update ExeptionHandler.java

--- demo/src/main/java/com/example/demo/ExeptionHandler.java
+++ demo/src/main/java/com/example/demo/ExeptionHandler.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
 * @module  : 模块描述
 * @comment : 模块包括的子功能
 * @Description: 异常处理
-* @author fanqingqing fanqingqing@sjq-it.com
+* @author fanqingqing 
 * @date 2017年6月19日 下午4:42:54
  */
 @ControllerAdvice
